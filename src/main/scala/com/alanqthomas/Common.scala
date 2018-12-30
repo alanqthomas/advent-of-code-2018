abb665117b19681c088d4b9c123f6ff5ebc62839
Alan Thomas <alan.q.thomas@gmail.com>
2018-12-30 15:20:44 -0500

    Add day 7 part 2

--- src/main/scala/com/alanqthomas/Common.scala
+++ src/main/scala/com/alanqthomas/Common.scala
@@ -4,14 +4,8 @@ import scala.io.Source
 import java.io.File
 
 object Common {
-  val windowsRootPath: String = new File("C:\\Users\\Alan\\Documents\\Projects\\advent-of-code-2018\\input").getAbsolutePath
-  val macRootPath: String = new File("/Users/alanqthomas/Documents/projects/advent-of-code-2018/input").getAbsolutePath
-
-  val isMac: Boolean = false
-
   def getLinesFromFile(filename: String): List[String] = {
-    val file = if (isMac) new File(s"$macRootPath/$filename")
-    else new File(s"$windowsRootPath\\$filename")
+    val file = new File(s"input/$filename")
 
     val it = for (
       line <- Source.fromFile(file).getLines
@@ -19,7 +13,5 @@ object Common {
 
     it.toList
   }
-
-
 
 }
--- src/main/scala/com/alanqthomas/Day7.scala
+++ src/main/scala/com/alanqthomas/Day7.scala
@@ -5,6 +5,7 @@ import scalax.collection.GraphPredef._
 import scalax.collection.GraphEdge._
 
 import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
 
 object Day7 extends App {
   val file = Common.getLinesFromFile("day7-input.txt")
@@ -28,6 +29,7 @@ object Day7 extends App {
     )
   }
 
+  // Part 1
   @tailrec
   def sortGraph(g: Graph[String, DiEdge], sorted: List[String] = List.empty): List[String] = {
     if (g.isEmpty) sorted
@@ -41,6 +43,92 @@ object Day7 extends App {
     }
   }
 
+  // Answer
   val sortedGraph = sortGraph(graph)
   println(s"sortedGraph = ${sortedGraph.mkString("")}")
+
+  // Part 2
+  def getPossibleWork(g: Graph[String, DiEdge]): Option[List[String]] =
+    getHeadLayer(g).map(_._2.toList.map(_.value))
+
+  case class Work(step: String, timeRemaining: Int)
+
+  val numberOfWorkers = 5
+  def workTimeForStep(step: String): Int = 60 + step.toCharArray.head - 64
+
+  val allCompleted: ListBuffer[String] = ListBuffer()
+
+  def makeNewWorkers(workers: Map[Int, Option[Work]], eligibleWork: List[String], eligibleWorkers: List[Int]): Map[Int, Option[Work]] = {
+    if (eligibleWork.isEmpty || eligibleWorkers.isEmpty) workers
+    else {
+      val sortedWork = eligibleWork.sorted
+      val worker = eligibleWorkers.head
+      val work = sortedWork.head
+
+      val newWorkers: Map[Int, Option[Work]] = workers + (worker -> Some(Work(work, workTimeForStep(work))))
+
+      makeNewWorkers(newWorkers, sortedWork.tail, eligibleWorkers.tail)
+    }
+  }
+
+  def totalWorkingTime(graph: Graph[String, DiEdge]): Int = {
+    @tailrec
+    def rec(g: Graph[String, DiEdge], workers: Map[Int, Option[Work]], currentTime: Int): Int = {
+      if (g.isEmpty && workers.values.forall(_.forall(_.timeRemaining == 0))) currentTime - 1
+      else {
+        var stepsCompleted: ListBuffer[String] = ListBuffer()
+
+        val updatedWorkers: Map[Int, Option[Work]] = workers.map {
+          case (id, Some(work)) =>
+            val newTimeRemaining = work.timeRemaining - 1
+
+            if (newTimeRemaining == 0) {
+              stepsCompleted += work.step
+              allCompleted += work.step
+              id -> None
+            } else id -> Some(work.copy(timeRemaining = newTimeRemaining))
+          case worker => worker
+        }
+
+        val newGraph = g -- stepsCompleted
+
+        val possibleWork = getPossibleWork(newGraph).map(_.toSet).getOrElse(Set.empty)
+        val currentlyWorking = updatedWorkers.values.flatMap(_.map(_.step)).toSet
+        val eligibleWork = possibleWork -- currentlyWorking
+
+        val eligibleWorkers = updatedWorkers.filter{ case (_, maybeWork) => maybeWork.isEmpty }.keys
+        val newWorkers = makeNewWorkers(Map.empty[Int, Option[Work]], eligibleWork.toList, eligibleWorkers.toList)
+        val idleWorkers = (eligibleWorkers.toSet -- newWorkers.keySet).map(id => id -> Option.empty[Work])
+        val allNewWorkers = updatedWorkers ++ newWorkers ++ idleWorkers
+        def workerStatus(id: Int): String = allNewWorkers(id).map(w => s"${w.timeRemaining} - ${w.step}").getOrElse(".")
+
+        if (false) {
+          println(String.format(
+            "|%6s|%8s|%8s|%8s|%8s|%8s|%-26s|",
+            currentTime.toString,
+            workerStatus(0),
+            workerStatus(1),
+            workerStatus(2),
+            workerStatus(3),
+            workerStatus(4),
+            allCompleted.mkString("")
+          ))
+        }
+
+        rec(newGraph, allNewWorkers, currentTime + 1)
+      }
+    }
+
+    println("--------------------------------------------------------------------------------")
+    println("|Second|Worker 1|Worker 2|Worker 3|Worker 4|Worker 5|Done                      |")
+    println("--------------------------------------------------------------------------------")
+    val workers = (0 until numberOfWorkers).map(_ -> Option.empty[Work]).toMap
+    rec(graph, workers, 0)
+  }
+
+  // Answer
+  val totalWorkTime = totalWorkingTime(graph)
+  println("--------------------------------------------------------------------------------")
+  println(s"totalWorkTime = ${totalWorkTime}")
+
 }
